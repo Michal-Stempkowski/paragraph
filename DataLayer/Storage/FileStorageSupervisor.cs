de3262d4997f62b268ca18cb01d352f6df9eb8cb
MichalStempkowski <michulix@gmail.com>
2015-06-07 22:48:31 +0200

    Before GUI

---
+++ DataLayer/Storage/FileStorageSupervisor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Reflection;
+
+namespace DataLayer.Storage
+{
+    public class FileStorageSupervisor : IStorageSupervisor
+    {
+        public readonly string RootPath;
+
+        public FileStorageSupervisor(string rootPath)
+        {
+            RootPath = rootPath;
+        }
+
+        public void Write(string path, string serializedRoom)
+        {
+            using (var file = new StreamWriter(path, append: false))
+            {
+                file.Write(serializedRoom);
+            }
+        }
+
+        public string Read(string path)
+        {
+            using (var file = new StreamReader(path))
+            {
+                return file.ReadToEnd();
+            }
+        }
+    }
+}
