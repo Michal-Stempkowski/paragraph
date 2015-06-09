9bb52bef4ecc5153f79285cc61ab0e28a87d51be
MichalStempkowski <michulix@gmail.com>
2015-06-09 22:04:25 +0200

    GUI stub

---
+++ GUI/Main.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GUI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.LoadGameButton = new System.Windows.Forms.Button();
+            this.EditorButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.NewGameButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.LoadGameButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.EditorButton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ExitButton, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(583, 544);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.NewGameButton.Location = new System.Drawing.Point(254, 3);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(75, 23);
+            this.NewGameButton.TabIndex = 0;
+            this.NewGameButton.Text = "NewGame";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
+            // 
+            // LoadGameButton
+            // 
+            this.LoadGameButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.LoadGameButton.Location = new System.Drawing.Point(254, 139);
+            this.LoadGameButton.Name = "LoadGameButton";
+            this.LoadGameButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.LoadGameButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadGameButton.TabIndex = 1;
+            this.LoadGameButton.Text = "LoadGame";
+            this.LoadGameButton.UseVisualStyleBackColor = true;
+            this.LoadGameButton.Click += new System.EventHandler(this.LoadGameButton_Click);
+            // 
+            // EditorButton
+            // 
+            this.EditorButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.EditorButton.Location = new System.Drawing.Point(254, 275);
+            this.EditorButton.Name = "EditorButton";
+            this.EditorButton.Size = new System.Drawing.Size(75, 23);
+            this.EditorButton.TabIndex = 2;
+            this.EditorButton.Text = "Editor";
+            this.EditorButton.UseVisualStyleBackColor = true;
+            this.EditorButton.Click += new System.EventHandler(this.EditorButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ExitButton.Location = new System.Drawing.Point(254, 411);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 568);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.Button LoadGameButton;
+        private System.Windows.Forms.Button EditorButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ GUI/Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GUI.Properties;
+
+namespace GUI
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadGameButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditorButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(Resources.ExitApplicationPrompt, Resources.ExitApplicationTitle,
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
